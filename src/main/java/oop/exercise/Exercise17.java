037060f1831a05125bc372e8e22891ac35d4c2e1
joshfeds14 <joshfeds14@gmail,com>
2021-06-05 12:42:02 -0400

    Exercise 17

--- src/main/java/oop/exercise/Exercise17.java
+++ src/main/java/oop/exercise/Exercise17.java
@@ -3,25 +3,7 @@ import java.util.Scanner;
 /*
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
  *  Copyright 2021 Joshua Federman
- Create a program that prompts for your weight, gender, total alcohol consumed (in ounces),
- * and the amount of time since your last drink. Calculate your blood alcohol content (BAC) using this formula
 
-BAC = (A × 5.14 / W × r) − .015 × H
-where
-
-A is total alcohol consumed, in ounces (oz).
-W is body weight in pounds.
-r is the alcohol distribution ratio:
-0.73 for men
-0.66 for women
-H is number of hours since the last drink.
-Display whether or not it’s legal to drive by comparing the blood alcohol content to 0.08.
-
-Example Output
-Your BAC is 0.08
-It is not legal for you to drive.
-Constraint
-Prevent the user from entering non-numeric values.
  */
 public class Exercise17
 {
@@ -29,13 +11,67 @@ public class Exercise17
     public static String getWeight() {
         System.out.print("What is your weight?");
         String weight = input.nextLine();
-        while(!weight.matches("[0-9]+")) {
+        while(!weight.matches("[0-9.]+"))
+        {
             System.out.print("Please enter a numerical value only.");
             weight = input.nextLine();
         }
         return weight;
     }
-    public static void main( String[] args ) {
+    public static String getGender() {
+        System.out.print("Press 1 if you are a man, Press 2 if you are a woman");
+        String gender = input.nextLine();
+        while(!gender.matches("[1]+") && !gender.matches("[2]+"))
+        {
+            System.out.print("Please enter either 1 or 2");
+            gender = input.nextLine();
+        }
+        if(gender.matches("1"))
+            return "0.73";
+        else
+            return "0.66";
+    }
+    public static String getAlcCons() {
+        System.out.print("How much alcohol have you consumed in ounces?");
+        String consume = input.nextLine();
+        while(!consume.matches("[0-9.]+"))
+        {
+            System.out.print("Please enter a numerical value only.");
+            consume = input.nextLine();
+        }
+        return consume;
+    }
+    public static String getHours() {
+        System.out.print("How many hours has it been since you have drank alcohol?");
+        String time = input.nextLine();
+        while(!time.matches("[0-9.]+"))
+        {
+            System.out.print("Please enter a numerical value only.");
+            time = input.nextLine();
+        }
+        return time;
+    }
+    public static double doMath(String w, String g, String c, String t){
+        float weight = Float.parseFloat(w);
+        float gender = Float.parseFloat(g);
+        float consumption = Float.parseFloat(c);
+        float time = Float.parseFloat(t);
+        double total = (consumption * 5.14 / weight * gender) - .015 * time;
+        return total;
+    }
+    public static void testDrive(double total){
 
+        if(total < 0.08)
+            System.out.print("It is legal for you to drive.");
+        else
+            System.out.print("It is not legal for you to drive.");
+    }
+    public static void main( String[] args ) {
+        String weight = getWeight();
+        String gender = getGender();
+        String consume = getAlcCons();
+        String time = getHours();
+        double total = doMath(weight,gender,consume,time);
+        testDrive(total);
     }
 }
