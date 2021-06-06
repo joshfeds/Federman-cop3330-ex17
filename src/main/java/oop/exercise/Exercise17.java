8c81c20831e9154aecd4d2f2674f5ac12596ed34
joshfeds14 <joshfeds14@gmail,com>
2021-06-06 17:34:37 -0400

    Exercise 17

--- src/main/java/oop/exercise/Exercise17.java
+++ src/main/java/oop/exercise/Exercise17.java
@@ -9,7 +9,7 @@ public class Exercise17
 {
     static Scanner input = new Scanner(System.in);
     public static String getWeight() {
-        System.out.print("What is your weight?");
+        System.out.print("What is your weight, in pounds? ");
         String weight = input.nextLine();
         while(!weight.matches("[0-9.]+"))
         {
@@ -19,11 +19,11 @@ public class Exercise17
         return weight;
     }
     public static String getGender() {
-        System.out.print("Press 1 if you are a man, Press 2 if you are a woman");
+        System.out.print("Enter a 1 is you are male or a 2 if you are female: ");
         String gender = input.nextLine();
         while(!gender.matches("[1]+") && !gender.matches("[2]+"))
         {
-            System.out.print("Please enter either 1 or 2");
+            System.out.print("Please enter either 1 or 2: ");
             gender = input.nextLine();
         }
         if(gender.matches("1"))
@@ -32,29 +32,29 @@ public class Exercise17
             return "0.66";
     }
     public static String getAlcCons() {
-        System.out.print("How much alcohol have you consumed in ounces?");
+        System.out.print("How many ounces of alcohol did you have? ");
         String consume = input.nextLine();
         while(!consume.matches("[0-9.]+"))
         {
-            System.out.print("Please enter a numerical value only.");
+            System.out.print("Please enter a numerical value only: ");
             consume = input.nextLine();
         }
         return consume;
     }
     public static String getHours() {
-        System.out.print("How many hours has it been since you have drank alcohol?");
+        System.out.print("How many hours has it been since your last drink? ");
         String time = input.nextLine();
         while(!time.matches("[0-9.]+"))
         {
-            System.out.print("Please enter a numerical value only.");
+            System.out.print("Please enter a numerical value only: ");
             time = input.nextLine();
         }
         return time;
     }
     public static double doMath(String w, String g, String c, String t){
         float weight = Float.parseFloat(w);
-        float gender = Float.parseFloat(g);
         float consumption = Float.parseFloat(c);
+        float gender = Float.parseFloat(g);
         float time = Float.parseFloat(t);
         double total = (consumption * 5.14 / weight * gender) - .015 * time;
         return total;
@@ -62,14 +62,14 @@ public class Exercise17
     public static void testDrive(double total){
 
         if(total < 0.08)
-            System.out.print("It is legal for you to drive.");
+            System.out.printf("Your BAC is %.6f\nIt is legal for you to drive.", total);
         else
-            System.out.print("It is not legal for you to drive.");
+            System.out.printf("Your BAC is %.6f\nIt is not legal for you to drive.",total);
     }
     public static void main( String[] args ) {
-        String weight = getWeight();
         String gender = getGender();
         String consume = getAlcCons();
+        String weight = getWeight();
         String time = getHours();
         double total = doMath(weight,gender,consume,time);
         testDrive(total);
